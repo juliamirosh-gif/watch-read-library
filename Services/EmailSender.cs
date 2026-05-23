5d6dc09e810ee0708742d71cb2b84d59f2cd468a
juliamirosh-gif <guliamirosh@gmail.com>
2026-05-23 19:43:27 +0300

    Disable email sending

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,43 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using System.Net;
-using System.Net.Mail;
 
 namespace WatchReadLibrary.Services
 {
     public class EmailSender : IEmailSender
     {
-        private readonly IConfiguration _configuration;
-
-        public EmailSender(IConfiguration configuration)
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            _configuration = configuration;
-        }
-
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            var smtpHost = _configuration["EmailSettings:SmtpHost"];
-            var smtpPort = int.Parse(_configuration["EmailSettings:SmtpPort"]!);
-            var smtpUser = _configuration["EmailSettings:SmtpUser"];
-            var smtpPassword = _configuration["EmailSettings:SmtpPassword"];
-            var fromEmail = _configuration["EmailSettings:FromEmail"];
-
-            using var client = new SmtpClient(smtpHost, smtpPort)
-            {
-                Credentials = new NetworkCredential(smtpUser, smtpPassword),
-                EnableSsl = true
-            };
-
-            var message = new MailMessage
-            {
-                From = new MailAddress(fromEmail!, "WatchReadLibrary"),
-                Subject = subject,
-                Body = htmlMessage,
-                IsBodyHtml = true
-            };
-
-            message.To.Add(email);
-
-            await client.SendMailAsync(message);
+            return Task.CompletedTask;
         }
     }
 }
